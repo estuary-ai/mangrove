ba43574be983cc94c84e9a5d65ca52394ccbc189
Aman Mohandas <aman.mohandas@gmail.com>
2022-03-13 12:12:33 -0700

    Added TTS
    Former-commit-id: ad74e4aa43a190f36b67ce1201cabff4f3764302

--- middleware_loaded.py
+++ middleware_loaded.py
@@ -1,11 +1,16 @@
 #Simulating how the frontend can speak to the Rasa server directly by loading the model
 
 import asyncio
+import pyttsx3
 
 from rasa.core.agent import Agent
 from rasa.utils.endpoints import EndpointConfig
 
 agent = Agent.load("models/20220312-161232-cyan-limit.tar.gz", action_endpoint=EndpointConfig('http://localhost:5055/webhook'))
+engine = pyttsx3.init()
+engine.setProperty('rate', 120)
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
 
 while True:
 
@@ -21,7 +26,10 @@ while True:
     #Printing the response
     for message in messages:
         if 'text' in message:
-            print (message['text'])
+            text = message['text']
+            print (text)
+            engine.say(text)
+            engine.runAndWait()
         if 'image' in message:
             print(message['image'])
         if 'custom' in message:
