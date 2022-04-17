ae815052b824d985c4f1a89274d4fdee0bffb182
Aman Mohandas <aman.mohandas@gmail.com>
2022-04-16 17:17:14 -0700

    Integrating socketIO with RASA
    Former-commit-id: 2c5afd369a398cac4079bea8a4d37b55dcf97600

--- stt-server-draft/stt_server.py
+++ stt-server-draft/stt_server.py
@@ -8,6 +8,32 @@ import time
 import numpy as np
 import sys
 
+
+'''==============================='''
+'''========= DM IMPORTS =========='''
+
+import asyncio
+import pyttsx3
+import os
+
+from rasa.core.agent import Agent
+from rasa.utils.endpoints import EndpointConfig
+
+'''==============================='''
+'''====== DM INITIALIZATION ======'''
+
+agent = Agent.load("models/model.tar.gz", action_endpoint=EndpointConfig('http://localhost:5055/webhook'))
+engine = pyttsx3.init()
+engine.setProperty('rate', 120)
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+if sys.platform == "win32" and (3, 8, 0) <= sys.version_info < (3, 9, 0):
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+'''==============================='''
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app,
@@ -151,7 +177,60 @@ def handle_stream_data(data):
     results = process_audio_stream(data)
     if results:
         print('results', results, flush=True)
-        socketio.emit('recognize', results)
+
+        #Assuming that results is a string containing transcripted speech
+        response = sendUserMessage({'Body': results, 'From': 'Neil'})       #get correct user ID
+
+        #Sending audio/commands/text back to Hololens
+        socketio.emit('recognize', response)
+
+'''==============================='''
+'''========== DM CODE ============'''
+
+def sendUserMessage(request):
+
+    user_message = request.get('Body')
+    conversation_id = request.get('From')
+
+    #Sending message to RASA
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    messages = loop.run_until_complete(agent.handle_text(user_message, sender_id=conversation_id))
+    loop.close()
+
+    #Deciphering RASA's response
+    audio_files = []
+    texts = []
+    commands = []
+    audio_present = False
+    
+    for message in messages:
+        if 'text' in message:
+            audio_present = True
+            text = message['text']
+            texts.append(text)
+            engine.save_to_file(text , f'output_{len(audio_files) + 1}.wav')
+            audio_files.append(f'output_{len(audio_files) + 1}.wav')
+            engine.runAndWait()
+        elif 'custom' in message:
+            commands.append(message['custom'])
+
+    #Compiling responses
+    response = {}
+    if audio_present:
+        audio_data = []
+        for audio_file in audio_files:
+            with open(audio_file, 'rb') as f:
+                audio_data.append(f.read())
+            os.remove(audio_file)
+        response['audio'] = audio_data
+
+    response['text'] = texts
+    response['commands'] = commands
+
+    return response
+
+'''==============================='''
 	
 @socketio.on('stream-end')
 def handle_stream_end():
