262f7ec4eef59e49bf0b6c409cc1e30052e7c397
Aman Mohandas <aman.mohandas@gmail.com>
2022-05-16 01:02:40 -0700

    snake_case FTW
    Former-commit-id: 2748f0c34b97fc366e2b0bb294807cccd39a1382

--- stt-server-draft/BotController.py
+++ stt-server-draft/BotController.py
@@ -12,7 +12,7 @@ class BotController:
         if sys.platform == "win32" and (3, 8, 0) <= sys.version_info < (3, 9, 0):
             asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-    def sendUserMessage(self, request):
+    def send_user_message(self, request):
 
         user_message = request.get('Body')
         conversation_id = request.get('From')
