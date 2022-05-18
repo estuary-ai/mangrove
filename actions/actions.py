f7776b1f7aea1a9e86a41b9e4c205c6d3c0fd11a
Aman Mohandas <aman.mohandas@gmail.com>
2022-05-17 23:34:41 -0700

    Exit Sample Tagging Flow Modification
    Former-commit-id: ea2d2c2cb034bfec71ebf6fc33f9e8c1d8e3a406

--- actions/actions.py
+++ actions/actions.py
@@ -11,7 +11,7 @@ from rasa_sdk.events import Restarted
 class ActionInitiateSample(Action):
 
     def name(self) -> Text:
-        return "action_initiate_sample"
+        return 'action_initiate_sample'
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         count = len(os.listdir('samples'))
@@ -75,30 +75,32 @@ class ValidateFormSampleTagging(FormValidationAction):
 class ActionSampleTagging(Action):
 
     def name(self) -> Text:
-        return "action_sample_tagging"
+        return 'action_sample_tagging'
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        count = len(os.listdir('samples'))
 
         #Using this condition to check if sample tagging form was filled or not
         if tracker.get_slot('geo_interpretation') is not None:
             sample_details = {
                 'Sample Details': {
-                    "Lighting Conditions": tracker.get_slot("lighting_conditions"),
-                    "Outcrop Appearance": tracker.get_slot("outcrop_appearance"),
-                    "Mechanism Used": tracker.get_slot("mechanism_used"),
-                    "Size and Shape": tracker.get_slot("size_and_shape"),
-                    "Sample Appearance": tracker.get_slot("sample_appearance"),
-                    "Geological Interpretation": tracker.get_slot("geo_interpretation")
+                    'Lighting Conditions': tracker.get_slot('lighting_conditions'),
+                    'Outcrop Appearance': tracker.get_slot('outcrop_appearance'),
+                    'Mechanism Used': tracker.get_slot('mechanism_used'),
+                    'Size and Shape': tracker.get_slot('size_and_shape'),
+                    'Sample Appearance': tracker.get_slot('sample_appearance'),
+                    'Geological Interpretation': tracker.get_slot('geo_interpretation')
                 }
             }
 
-            count = len(os.listdir("samples"))
             json.dump(sample_details['Sample Details'], open(f'samples/sample_{count}/sample_details.json', 'w'))
             sample_details['File Name'] = f'sample_{count}/sample_details.json'
             dispatcher.utter_message(custom=sample_details)
-            dispatcher.utter_message(response="utter_sample_tagged")
+            dispatcher.utter_message(response='utter_sample_tagged')
 
         else:
-            dispatcher.utter_message(response="utter_exit")
+            dispatcher.utter_message(response='utter_exit')
 
+        dispatcher.utter_message(custom={'sample': False, 'id': count})
         return [Restarted()]
