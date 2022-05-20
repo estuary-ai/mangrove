28ed975660b68448a41fc7da411b16a68b23bca1
basem.rizk@outlook.com <basem.rizk@outlook.com>
2022-05-19 19:41:44 -0500

    add models to push
    Former-commit-id: 6ea284bf8aea88e6aa22544c3f9def1e89568ce7

---
+++ models/ds-model/download.sh
@@ -0,0 +1,3 @@
+# Download pre-trained English model files
+curl -LO https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.pbmm
+curl -LO https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.scorer
