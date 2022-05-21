06fa87f6fe21704a2fb3c00b5f8e191c527bc6e3
basem.rizk@outlook.com <basem.rizk@outlook.com>
2022-05-21 17:45:00 -0500

    middleware_bot using botcontroller debug
    Former-commit-id: 40a05d0774c38ef7df4a5753da6c71ad49679222

--- debug-and-drafts/middleware_bot.py
+++ debug-and-drafts/middleware_bot.py
@@ -7,50 +7,26 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 import asyncio
 from flask import Flask, request
-import pyttsx3
-
-from rasa.core.agent import Agent
-from rasa.utils.endpoints import EndpointConfig
-
-agent = Agent.load("../models/rasa-model/model.tar.gz", action_endpoint=EndpointConfig('http://localhost:5055/webhook'))
-engine = pyttsx3.init()
-engine.setProperty('rate', 120)
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
+from BotController import BotController
 
 app = Flask(__name__)
 
 if sys.platform == "win32" and (3, 8, 0) <= sys.version_info < (3, 9, 0):
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
+bot = BotController(model_path='../models/rasa-model/model.tar.gz')
+
 @app.route("/", methods=["POST"])
 def sendUserMessage():
 
     user_message = request.values.get('Body')
-    conversation_id = request.values.get('From')
-
+    # conversation_id = request.values.get('From')
     if user_message == 'stop':
         return "End"
 
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    messages = loop.run_until_complete(agent.handle_text(user_message, sender_id=conversation_id))
-    loop.close()
-    print("printing response")
-    #Printing the response
-    # print(messages)
-    for message in messages:
-        if 'text' in message:
-            text = message['text']
-            # print(text)
-            engine.save_to_file(text , 'output.wav')
-            engine.runAndWait()
-        # elif 'custom' in message:
-        #     print(message['custom'])
-        
-        print("\nmessage:", message)
-
-    return "Success"
+    bot_res = bot.send_user_message(user_message)
+    print("EVA:", bot_res)
+    return bot_res
 
 if __name__ == '__main__':
     app.run()
