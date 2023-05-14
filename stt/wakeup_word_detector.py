b01307e6f53f1bae3fbdc13049d596487e5c82b7
basrizk <basem.rizk@outlook.com>
2023-05-14 01:48:12 -0700

    Heavy Refactoring of STT particularly and major updates: - Creating Producer Consumer Architecture for handling stt response and what follows - Using AudioPackets instead of bare bytes - Using thread-safe DataBuffer for AudioPackets - Using DataBufer for WakeUpWord and STTController - TTSController slight tuning and adding reading plain audio
    Former-commit-id: 88a4455782ff1c06ebafdff45981f7538611d861

--- stt/wakeup_word_detector.py
+++ stt/wakeup_word_detector.py
@@ -1,15 +1,14 @@
-from threading import Thread
 import pvporcupine
 import sys
 import struct
+from threading import Thread
+from .data_buffer import DataBuffer
 
 class WakeUpVoiceDetector(Thread):
 
     def __init__(
             self,
-            # access_key = "2aUEVaMWbCb6t+Q/o87vQtMX+ITUoMB65242NNDC3qtXxxbwIkzqEA==",
             access_key = "9DG6ZfAltia6TEuOiU/IImtAsttIfCeyxpks3SuNVbOY8LWkbOSjHQ==",
-            # keyword_paths = ["models/pvprocupine/Hello-eva_en_windows_v2_1_0.ppn"],
             keyword_paths = [
                 [
                     "models/pvprocupine/sen-va_en_windows_v2_2_0/sen-va_en_windows_v2_2_0.ppn",
@@ -18,6 +17,8 @@ class WakeUpVoiceDetector(Thread):
                 ["models/pvprocupine/sen-va_en_linux_v2_2_0/sen-va_en_linux_v2_2_0.ppn"]
             ],
             sensitivities=None):
+        super(WakeUpVoiceDetector, self).__init__()
+
         try:
             if sys.platform.startswith('linux'):
                 keyword_paths = keyword_paths[1]
@@ -29,43 +30,32 @@ class WakeUpVoiceDetector(Thread):
         except:
             raise Exception("Unsupported Platform")    
 
-        super(WakeUpVoiceDetector, self).__init__()
         self._access_key = access_key
-        # self._library_path =pvporcupine.LIBRARY_PATH
-        # self._model_path = pvporcupine.MODEL_PATH
         self._keyword_paths = keyword_paths
         self._sensitivities = sensitivities
 
-        self.porcupine = pvporcupine.create(
+        self.porcupine_handle = pvporcupine.create(
                 access_key=self._access_key,
-                # library_path=self._library_path,
-                # model_path=self._model_path,
                 keyword_paths=self._keyword_paths,
                 sensitivities=self._sensitivities)
 
         self.frame_size = 1024
-        self.reset_data_buffer()
-
+        self.buffer = DataBuffer(self.frame_size)
+        # self.reset_data_buffer()
 
     def reset_data_buffer(self):
-        self.buffered_data = b""
+        self.buffer.reset()
 
-    def process_audio_stream(self, new_data): 
-        data_stream = self.buffered_data + new_data 
-        self.reset_data_buffer()
-
-        i = 0
-        while i < len(data_stream):
-            sub_data = data_stream[i:i+self.frame_size]
+    def process_audio_stream(self, audio_packet): 
+        # Utilize full audio_packet instead
+        self.buffer.add(audio_packet)
+        for frame in self.buffer:
             # Process only proper frame sizes
-            if len(sub_data) < self.frame_size:
+            if len(frame) < self.frame_size:
                 break
             # print("\nProcessing", flush=True)
-            pcm = struct.unpack_from("h" *self.porcupine.frame_length, sub_data)
-            result = self.porcupine.process(pcm)
+            pcm = struct.unpack_from("h" *self.porcupine_handle.frame_length, frame.bytes)
+            result = self.porcupine_handle.process(pcm)
             if result >= 0:
                 self.reset_data_buffer()
                 return True
-            i += self.frame_size
-        
-        self.buffered_data = data_stream[i:]
