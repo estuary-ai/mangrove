86a095eac36275375810de761267756ed940d48f
Athang Gupte <xdevapps0@gmail.com>
2023-05-14 22:07:53 -0700

    Updated exchange format. Updated commands list to reflect latest requirements
    Former-commit-id: 60f53783a96b059f254cd415ec2af67c075aad55

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -14,36 +14,84 @@ class ActionShowPanel(Action):
         return 'action_show_panel'
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
         panel = tracker.get_slot('panel')
         switch_ = tracker.get_slot('switch_')
-        if switch_ is None:
+        if switch_ is None or switch_ == 'on' or switch_ != 'off':
             switch_ = 'on'
-        elif switch_ not in ['on', 'off']:
-            panel = switch_
-            switch_ = 'on'
+            text='Opening up the %s panel' % panel
+        else:
+            text='Closing the %s panel' % panel
 
-        dispatcher.utter_message(text='Opening up the %s panel' % panel, custom={'toggle': True, 'feature': panel, 'switch_': switch_})
+        cmd = { 'target': panel, 'action': 'set', 'additionalInfo': [switch_] }
+
+        dispatcher.utter_message(text=text, custom=cmd)
 
         return [Restarted()]
 
-class ActionNavigationText(Action):
+class ActionNavigation(Action):
 
     def name(self) -> Text:
-        return 'action_navigation_text'
+        return 'action_navigation'
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         switch_ = tracker.get_slot('switch_')
-        if switch_ is None or switch_ == 'on':
-            dispatcher.utter_message(text='Opening up the navigation menu for you')
+
+        if switch_ is None or switch_ == 'on' or switch_ != 'off':
+            switch_ = 'on'
+            text='Opening up the navigation menu for you'
         else:
-            dispatcher.utter_message(text='Closing the navigation menu for you')
+            text='Closing the navigation menu for you'
+
+        cmd = { 'target': 'Navigation', 'action': 'set', 'additionalInfo': [switch_] }
+
+        dispatcher.utter_message(text=text, custom=cmd)
         return [Restarted()]
+
+
+class ActionShortRangeNavigation(Action):
+
+    def name(self) -> Text:
+        return 'action_short_range_navigation'
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        switch_ = tracker.get_slot('switch_')
+
+        if switch_ is None or switch_ == 'on' or switch_ != 'off':
+            switch_ = 'on'
+            text='Showing short range navigation markers'
+        else:
+            text='Hiding short range navigation markers'
+
+        cmd = { 'target': 'ShortRangeNavigation', 'action': 'set', 'additionalInfo': [switch_] }
+
+        dispatcher.utter_message(text=text, custom=cmd)
+        return [Restarted()]
+
+
+class ActionShortRangeNavSettings(Action):
+
+    def name(self) -> Text:
+        return 'action_short_nav_settings'
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        switch_ = tracker.get_slot('switch_')
+
+        if switch_ is None or switch_ == 'on' or switch_ != 'off':
+            switch_ = 'on'
+            text='Opening up the short range navigation settings'
+        else:
+            text='Closing the short range navigation settings'
+
+        cmd = { 'target': 'ShortRangeNavigationSettings', 'action': 'set', 'additionalInfo': [switch_] }
+
+        dispatcher.utter_message(text=text, custom=cmd)
+        return [Restarted()]
+
 
 class ActionTerrainText(Action):
     
     def name(self) -> Text:
-        return 'action_terrain_text'
+        return 'action_terrain'
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         switch_ = tracker.get_slot('switch_')
@@ -52,3 +100,9 @@ class ActionTerrainText(Action):
         else:
             dispatcher.utter_message(text='Hiding terrain markers')
         return [Restarted()]
+
+class ActionMap(Action):
+    
+    def name(self) -> Text:
+        return 'action_map'
+
