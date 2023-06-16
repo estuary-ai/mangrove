3a52d4ec8730ccb3402aaab7457bc22f488ad50b
basrizk <basem.rizk@outlook.com>
2023-06-15 22:13:00 -0700

    adding simple python client
    Former-commit-id: db70e6393a2c0e8d268603d1614fea31994fc34f

---
+++ client/python/misc.py
@@ -0,0 +1,23 @@
+def setup_terminate_signal_if_win(close_callback=None): 
+    """ Setup a signal handler for windows to catch Ctrl+C
+    """  
+    import sys
+    is_windows = sys.platform.startswith('win')
+    if not is_windows:
+        return
+    
+    from engineio.client import signal_handler    
+    from win32api import SetConsoleCtrlHandler
+    def handler(event):
+        import inspect
+        import signal
+        if event == 0:
+            try:
+                if close_callback: close_callback()
+                signal_handler(signal.SIGINT, inspect.currentframe())
+            except:
+                # SetConsoleCtrlHandler handle cannot raise exceptions
+                pass
+            
+    SetConsoleCtrlHandler(handler, 1)
+    
