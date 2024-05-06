dc3374eca8db7c6f2a6485135fb7d78b2e3f87e8
Basem Rizk <basem.rizk@outlook.com>
2024-05-06 00:19:12 -0700

    xtts to stream audio dict + updating requirements

--- tts/endpoints/xtts.py
+++ tts/endpoints/xtts.py
@@ -1,21 +1,43 @@
 import io
 import os
+import time
 import scipy
 import torch
 import numpy as np
-from pydub import AudioSegment
+from TTS.tts.configs.xtts_config import XttsConfig, XttsAudioConfig
+from TTS.tts.models.xtts import Xtts
 from TTS.api import TTS
+from pydub import AudioSegment
 from loguru import logger
 from .base import TTSEndpoint, audio_segment_to_audio_bytes_dict
 from tts.endpoints.elevenlabs import ElevenLabsTTSEndpoint
 
 class TTSLibraryEndpoint(TTSEndpoint):
-    def __init__(self,  device=None, **kwargs):
+    def __init__(
+        self,
+        model_name="tts_models/multilingual/multi-dataset/xtts_v2",
+        device=None,
+        **kwargs
+    ):
         if device is None:
             device = "cuda" if torch.cuda.is_available() else "cpu"
 
-        self.engine = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+        ckpt_dir = TTS().download_model_by_name(model_name)[-1]
+        config_path = os.path.join(ckpt_dir, "config.json")
+        if not os.path.exists(config_path):
+            raise ValueError(f"Config file not found at {config_path}")
+
+        config = XttsConfig()
+        config.load_json(config_path)
+        model = Xtts.init_from_config(config)
+        model.load_checkpoint(config, checkpoint_dir=ckpt_dir, use_deepspeed=True)
+        if device == "cuda":
+            model.cuda()
         self._ensure_speaker_wav()
+        print("Computing speaker latents...")
+        self.gpt_cond_latent, self.speaker_embedding = model.get_conditioning_latents(audio_path=["speaker.wav"])
+        self.model = model
+        self.sample_rate = XttsAudioConfig.output_sample_rate
 
     def _ensure_speaker_wav(self):
         if not os.path.exists('speaker.wav'):
@@ -28,17 +50,36 @@ class TTSLibraryEndpoint(TTSEndpoint):
             )
 
     def text_to_audio_file(self, text, filepath):
-        self.engine.tts_to_file(text=text, file_path=filepath, speaker_wav="speaker.wav", language="en")
+        raise NotImplementedError("This method is not implemented for TTSLibraryEndpoint")
+    #     self.engine.tts_to_file(text=text, file_path=filepath, speaker_wav="speaker.wav", language="en")
 
     def text_to_bytes(self, text):
-        def get_audio_segment():
-            wav_audio = np.array(self.engine.tts(text=text, speaker_wav="speaker.wav", language="en"))
-            sample_rate = self.engine.synthesizer.output_sample_rate
-            wav_norm = wav_audio * (32767 / max(0.01, np.max(np.abs(wav_audio))))
-            wav_norm = wav_norm.astype(np.int16)
-            wav_buffer = io.BytesIO()
-            scipy.io.wavfile.write(wav_buffer, sample_rate, wav_norm)
-            wav_buffer.seek(0)
-            return AudioSegment.from_file(wav_buffer, format="wav")
+        def audio_segments_generator():
+            def wav_audio_to_audio_segment(wav_audio):
+                # wav_audio = np.array(self.engine.tts(text=text, speaker_wav="speaker.wav", language="en"))
+                wav_norm = wav_audio * (32767 / max(0.01, np.max(np.abs(wav_audio))))
+                wav_norm = wav_norm.astype(np.int16)
+                wav_buffer = io.BytesIO()
+                scipy.io.wavfile.write(wav_buffer, self.sample_rate, wav_norm)
+                wav_buffer.seek(0)
+                return AudioSegment.from_file(wav_buffer, format="wav")
 
-        return audio_segment_to_audio_bytes_dict(get_audio_segment())
+            print("Inference...")
+            t0 = time.time()
+            chunks = self.model.inference_stream(
+                text,
+                "en",
+                self.gpt_cond_latent,
+                self.speaker_embedding
+            )
+
+            for i, chunk in enumerate(chunks):
+                if i == 0:
+                    print(f"Time to first chunck: {time.time() - t0}")
+                print(f"Received chunk {i} of audio length {chunk.shape[-1]}")
+                yield wav_audio_to_audio_segment(chunk.cpu().numpy())
+
+
+
+        for audio_segment in audio_segments_generator():
+            yield audio_segment_to_audio_bytes_dict(audio_segment)
