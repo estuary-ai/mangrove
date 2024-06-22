573975e86f5c75f64993507e3b60c1be5edae2a1
Basem Rizk <basem.rizk@outlook.com>
2024-06-22 04:45:32 -0700

    remove hardcode unused values from pyttsx3 endpoint

--- tts/endpoints/pyttsx3.py
+++ tts/endpoints/pyttsx3.py
@@ -3,11 +3,12 @@ from .base import TTSEndpoint, get_mp3_audio_bytes
 
 class Pyttsx3TTSEndpoint(TTSEndpoint):
     def __init__(self, voice_rate=100, voice_id=12, **kwargs):
+        # MAKE IT SINGLETON AS PYTTSX3 DOESN'T SUPPORT MULTIPLE INSTANCES
         import pyttsx3
         self.engine = pyttsx3.init(debug=True)
-        self.sample_width = 2
-        self.channels = 1
-        self.frame_rate = 22050
+        # self.sample_width = 2
+        # self.channels = 1
+        # self.frame_rate = 22050
         self.engine.setProperty("rate", voice_rate)
         voices = self.engine.getProperty("voices")
         # voice_str = "\n".join(voices)
