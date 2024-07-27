9b4ee0d8ac336c9c0816e0ca3e33de5026d7edfb
basrizk <basem.rizk@outlook.com>
2024-07-27 15:45:03 -0700

    update elevenlabs calls and implement its stream_bytes

--- tts/endpoints/elevenlabs.py
+++ tts/endpoints/elevenlabs.py
@@ -1,28 +1,20 @@
 import os
-from elevenlabs import generate, save, set_api_key, voices
+from elevenlabs.client import ElevenLabs
 from .base import TTSEndpoint
 
 class ElevenLabsTTSEndpoint(TTSEndpoint):
-    def __init__(self, model='eleven_multilingual_v2', **kwargs):
-        set_api_key(os.environ['ELEVENLABS_API_KEY'])
-        self.voice = voices()[0]
-        self.model = model
+    def __init__(self, model_name='eleven_multilingual_v2', **kwargs):
+        self.client = ElevenLabs(api_key=os.environ['ELEVENLABS_API_KEY'])
+        self.model_name = model_name
 
     def text_to_audio_file(self, text, filepath):
-        save(self.text_to_bytes(text), filepath)
+        _bytes = self.text_to_bytes(text)
+        with open(filepath, 'wb') as f:
+            for chunk in _bytes:
+                f.write(chunk)
 
     def text_to_bytes(self, text):
-        return generate(text=text, voice=self.voice, model=self.model)
+        return self.client.generate(text=text, model=self.model_name)
 
     def stream_bytes(self, text):
-        # def get_audio_bytes():
-        #     return generate(text=text, voice=self.voice, model=self.model, stream=True)
-        # breakpoint()
-        # # TODO: implement stream_bytes
-        # return {
-        #     'audio_bytes': get_audio_bytes(),
-        #     'frame_rate': 24000,
-        #     'sample_width': 2,
-        #     'channels': 1
-        # }
-        raise NotImplementedError("stream_bytes for ElevenLabsTTSEndpoint not implemented yet")
+        return self.client.generate(text=text, model=self.model_name, stream=True)
